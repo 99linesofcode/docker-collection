4f2456efa59fe163baf9c72e3d2be4154df8b781
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2020-07-02 13:28:54 +0200

    :construction_worker: Made provision script interactive

--- provision.sh
+++ provision.sh
@@ -20,52 +20,59 @@ fi
 echo "All prerequisites installed. Continuing.."
 cd ~/current
 
-if ! [ -f .env ]; then
-  echo "Missing Docker dotenv file. Please copy and configure env-example before continuing."
+if ! [ -f .env.example ]; then
+  echo "$PWD/.env.example is missing. Cannot continue.."
   exit
-else
-  echo "Sourcing Docker dotenv file.."
-  source .env
+fi
 
-  if [ -z $REPOSITORY ]; then
-    echo '$REPOSITORY not set. Please define the repository that should be pulled.'
+if ! [ -f .env ]; then
+  echo "Copying .env from $PWD/.env.example.."
+  cp .env.example .env
+fi
+
+source .env
+
+if [ -z $REPOSITORY ]; then
+  echo '$REPOSITORY not defined. What repository do you want to pull?'
+  read -p 'git@github.com:99linesofcode/' repo
+  sed -i "s/REPOSITORY=.*/REPOSITORY=$repo/g" .env
+fi
+
+if [ -n $(find $PWD/config/nginx/www -maxdepth 0 -type d -empty 2>/dev/null) ]; then
+  echo "Pulling $REPOSITORY from github.com.."
+  mkdir -p $PWD/config/nginx/www
+  git clone git@github.com:99linesofcode/$REPOSITORY.git $PWD/config/nginx/www
+fi
+
+if [ -z $(docker ps -q -f name=app) ]; then
+  echo "Starting Docker containers.."
+  docker-compose -f docker-compose.yml up -d
+else
+  echo "Restarting Docker containers.."
+  docker-compose down
+  docker-compose -f docker-compose.yml up -d
+fi
+
+if grep -q "laravel/framework" $PWD/config/nginx/www/composer.json; then
+  echo "$REPOSITORY looks to be a Laravel project. Configuring.."
+
+  if [ ! -f $PWD/config/nginx/www/.env ] && [ -f $PWD/config/nginx/www/.env.example ]; then
+    cp $PWD/config/nginx/www/.env.example $PWD/config/nginx/www/.env
+  else
+    echo "$PWD/config/nginx/www/.env.example is missing. Make sure it exists before continuing."
     exit
   fi
 
-  if [ -n $(find $PWD/config/nginx/www -maxdepth 0 -type d -empty 2>/dev/null) ]; then
-    echo "Project not yet initialized. Pulling $REPOSITORY from github.com.."
-    mkdir -p $PWD/config/nginx/www
-    git clone git@github.com:99linesofcode/$REPOSITORY.git $PWD/config/nginx/www
-  else
-    echo "Project was previously pulled. Continuing.."
+  echo "Installing Composer dependencies.."
+  docker run --rm -t -v $PWD/config/nginx/www:/app composer install
+
+  if [ -z $(sed -n 's/^APP_KEY=//p' $PWD/config/nginx/www/.env) ]; then
+    echo "Application key not set. Generating a new one.."
+    docker exec app php artisan key:generate
   fi
 
-  if [ -z $(docker ps -q -f name=app) ]; then
-    echo "Starting Docker containers.."
-    docker-compose -f docker-compose.yml up -d
-  else
-    echo "Restarting Docker containers.."
-    docker-compose down
-    docker-compose -f docker-compose.yml up -d
-  fi
-
-  if grep -q "laravel/framework" $PWD/config/nginx/www/composer.json; then
-    if [ ! -f $PWD/config/nginx/www/.env ] && [ -f $PWD/config/nginx/www/.env.example ]; then
-      cp $PWD/config/nginx/www/.env.example $PWD/config/nginx/www/.env
-      docker run --rm -t -v $PWD/config/nginx/www:/app composer install
-      docker run --rm -t -w /app -v $PWD/config/nginx/www:/app node:14-alpine sh -c "npm install && npm run prod"
-
-      if [ -z $(sed -n 's/^APP_KEY=//p' $PWD/config/nginx/www/.env) ]; then
-        echo "Generating Laravel application key.."
-        docker exec app php artisan key:generate
-      fi
-    else
-      echo "Could not find .env.example to copy. Aborting.."
-      exit
-    fi
-  else
-    echo "$PWD/config/nginx/www does not appear to contain a Laravel project. Consider manual configuration."
-  fi
+  echo "Installing Vue dependencies.."
+  docker run --rm -t -w /app -v $PWD/config/nginx/www:/app node:14-alpine sh -c "npm install --production && npm run prod"
 fi
 
 echo "Done. All fired up and ready to serve!"
