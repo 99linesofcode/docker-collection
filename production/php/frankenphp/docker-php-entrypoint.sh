bba71e0cc3e39fed28ccfc9ebc8c7cc31847bb7a
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-06-04 13:08:38 +0200

    refactor!: move and pull code from dedicated submodules
    This is a major overhaul of the folder structure. From now on this
    repository will serve as a reference and collection of all my Docker
    files. Modules herein should instead be pulled into your project as a
    submodule. The instructions for that can be found in the module's
    repository README file.
    
    BREAKING_CHANGE

--- production/php/frankenphp/docker-php-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# TODO: add deploy:setup command on first deployment
-
-# TODO: add deploy:finalize command to handle post deployment (artisan down, a migrate, a optimize, etc.)
-
-exec "$@"
